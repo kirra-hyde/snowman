af40204084b0a1ced87ed231cbeaa32b58503e99
Kirra Hyde <kirra.hyde@gmail.com>
2023-08-23 18:22:41 -0400

    WIP test

---
+++ src/Snowman.test.js
@@ -0,0 +1,10 @@
+import Snowman from "./Snowman";
+import React from "react";
+import {render, fireEvent} from @testing-library/react;
+
+it("Ends game after 6 wrong guesses", function() {
+  const { container } = render(<Snowman/>);
+
+  //click letter
+  const b = ______.b
+})
